557d04b8fdef0da151214186f9425e77934f30e7
Dan <daniel.liemng@gmail.com>
2022-04-18 22:17:37 -0400

    possibility

--- src/containers/possibility/Possibility.js
+++ src/containers/possibility/Possibility.js
@@ -1,8 +1,29 @@
 import React from 'react';
-import './possibility.module.scss';
+import './possibility.scss';
+import possibility from '../../assets/possibility.png';
 
 const Possibility = () => {
-  return <div>Possibility</div>;
+  return (
+    <div className='gpt__possibility section__padding'>
+      <div className='gpt__possibility-image'>
+        <img src={possibility} alt='possibility' />
+      </div>
+
+      <div className='gpt__possibility-content'>
+        <h4>Request Early Access to Get Started</h4>
+        <h1 className='gradient__text'>
+          The possibilities are <br /> beyond your imagination
+        </h1>
+        <p>
+          Yet bed any for travelling assistance indulgence unpleasing. Not
+          thoughts all exercise blessing. Indulgence way everything joy
+          alteration boisterous the attachment. Party we years to order allow
+          asked of.
+        </p>
+        <h4>Request Early Access to Get Started</h4>
+      </div>
+    </div>
+  );
 };
 
 export default Possibility;
