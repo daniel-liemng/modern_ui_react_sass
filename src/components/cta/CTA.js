91e195efc3003ec439e908438727d417a0cb0561
Dan <daniel.liemng@gmail.com>
2022-04-18 22:50:43 -0400

    cta

--- src/components/cta/CTA.js
+++ src/components/cta/CTA.js
@@ -1,8 +1,19 @@
 import React from 'react';
-import './cta.module.scss';
+import './cta.scss';
 
 const CTA = () => {
-  return <div>CTA</div>;
+  return (
+    <div id='cta' className='gpt__cta'>
+      <div className='gpt__cta-content'>
+        <p>Request Early Access to Get Started</p>
+        <h3>Register Today & start exploring the endless possibilities.</h3>
+      </div>
+
+      <div className='gpt__cta-button'>
+        <button type='button'>Get Started</button>
+      </div>
+    </div>
+  );
 };
 
 export default CTA;
