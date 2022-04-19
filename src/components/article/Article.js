27fb3a6646e2253fa27e51937d23b5fbeba746b2
Dan <daniel.liemng@gmail.com>
2022-04-18 23:30:10 -0400

    blog

--- src/components/article/Article.js
+++ src/components/article/Article.js
@@ -1,8 +1,21 @@
 import React from 'react';
-import './article.module.scss';
+import './article.scss';
 
-const Article = () => {
-  return <div>Article</div>;
+const Article = ({ imgUrl, text, date }) => {
+  return (
+    <div className='gpt__blog-container_article'>
+      <div className='gpt__blog-container_article-image'>
+        <img src={imgUrl} alt='blog' />
+      </div>
+      <div className='gpt__blog-container_article-content'>
+        <div>
+          <p>{date}</p>
+          <h3>{text}</h3>
+        </div>
+        <p>Read Full Article</p>
+      </div>
+    </div>
+  );
 };
 
 export default Article;
